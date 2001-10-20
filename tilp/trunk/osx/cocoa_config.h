adcec51062d45b9cd248946386851850da7129da
Julien Blache <jb@jblache.org>
2001-10-20 15:53:43 +0000

    UserDefaults support
    git-svn-id: http://svn.tilp.info/repos/tilp@19

---
+++ tilp/trunk/osx/cocoa_config.h
@@ -0,0 +1,18 @@
+//
+//  cocoa_config.h
+//  TiLP
+//
+//  Created by julien on Sat Oct 20 2001.
+//  Copyright (c) 2001 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef __COCOA_CONFIG_H__
+#define __COCOA_CONFIG_H__
+
+void
+read_rc_file(void);
+
+void
+write_rc_file(void);
+
+#endif /* !__COCOA_CONFIG_H__ */
