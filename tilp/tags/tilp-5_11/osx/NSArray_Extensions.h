d6bf8d73f4bb2c3230b2cf18c7f584b4335d552b
cvs2svn conversion <cvs2svn@tilp.info>
2003-04-06 13:56:37 +0000

    16 copies to tags/branches
    git-svn-id: http://svn.tilp.info/repos/tilp@87

---
+++ tilp/tags/tilp-5_11/osx/NSArray_Extensions.h
@@ -0,0 +1,37 @@
+/*  TiLP - Linking program for TI calculators
+ *  Copyright (C) 2001-2002 Julien BLACHE <jb@technologeek.org>
+ *
+ *  Cocoa GUI for Mac OS X
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+/*
+ * This code is derived from Apple Sample Code (DragNDropOutlineView)
+ * Apple is not liable for anything regarding this code, according to
+ * the Apple Sample Code License.
+ *
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (MyExtensions)
+- (BOOL)containsObjectIdenticalTo: (id)object;
+@end
+
+@interface NSMutableArray (MyExtensions)
+- (void) insertObjectsFromArray:(NSArray *)array atIndex:(int)index;
+@end
+
---
+++ tilp/tags/tilp-5_11/osx/cocoa_refresh.h
@@ -0,0 +1,29 @@
+/*  TiLP - Linking program for TI calculators
+ *  Copyright (C) 2001-2002 Julien BLACHE <jb@technologeek.org>
+ *
+ *  Cocoa GUI for Mac OS X
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __COCOA_REFRESH_H__
+#define __COCOA_REFRESH_H__
+ 
+void gt_refresh_pbar();
+void gt_refresh_label();
+void gt_refresh_gui();
+void gt_init_refresh_functions(void);
+
+#endif /* !__COCOA_REFRESH_H__ */
