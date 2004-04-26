ce25bf88959f21d34e41ddac3071eabd62553de4
Romain Liévin <roms@lievin.net>
2004-04-26 13:42:15 +0000

    Clean-up.
    git-svn-id: http://svn.tilp.info/repos/tilp@653

--- tilp/trunk/src/toolbar.c
+++ tilp/trunk/src/toolbar.c
@@ -25,7 +25,9 @@
 
 #include "gstruct.h"
 #include "support.h"
+
 struct toolbar_window toolbar_win = { 0 };
+
 void toolbar_set_images(void)
 {
 	GtkToolbar *tb = GTK_TOOLBAR(toolbar_win.toolbar);
@@ -33,53 +35,58 @@ void toolbar_set_images(void)
 	GtkWidget *old_icon, *new_icon;
 	GdkPixbuf *pixbuf;
 
-//gdk_pixbuf_new_from_xpm_data    (const char **data);
+	//gdk_pixbuf_new_from_xpm_data    (const char **data);
 	if (toolbar_win.toolbar == NULL)
 		return;
+		
 	child = g_list_nth(tb->children, 0)->data;
 	old_icon = child->icon;
 	new_icon = create_pixmap(toolbar_win.toolbar, "tb_ready.xpm");
 	pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(new_icon));
 	gtk_image_set_from_pixbuf(GTK_IMAGE(old_icon), pixbuf);
+	
 	child = g_list_nth(tb->children, 1)->data;
 	old_icon = child->icon;
-	new_icon =
-	    create_pixmap(toolbar_win.toolbar, "tb_calc_screen.xpm");
+	new_icon = create_pixmap(toolbar_win.toolbar, "tb_calc_screen.xpm");
 	pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(new_icon));
 	gtk_image_set_from_pixbuf(GTK_IMAGE(old_icon), pixbuf);
+	
 	child = g_list_nth(tb->children, 2)->data;
 	old_icon = child->icon;
-	new_icon =
-	    create_pixmap(toolbar_win.toolbar, "tb_calc_dirlist.xpm");
+	new_icon = create_pixmap(toolbar_win.toolbar, "tb_calc_dirlist.xpm");
 	pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(new_icon));
 	gtk_image_set_from_pixbuf(GTK_IMAGE(old_icon), pixbuf);
+	
 	child = g_list_nth(tb->children, 3)->data;
 	old_icon = child->icon;
-	new_icon =
-	    create_pixmap(toolbar_win.toolbar, "tb_calc_backup.xpm");
+	new_icon = create_pixmap(toolbar_win.toolbar, "tb_calc_backup.xpm");
 	pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(new_icon));
 	gtk_image_set_from_pixbuf(GTK_IMAGE(old_icon), pixbuf);
+	
 	child = g_list_nth(tb->children, 4)->data;
 	old_icon = child->icon;
-	new_icon =
-	    create_pixmap(toolbar_win.toolbar, "tb_calc_restore.xpm");
+	new_icon = create_pixmap(toolbar_win.toolbar, "tb_calc_restore.xpm");
 	pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(new_icon));
 	gtk_image_set_from_pixbuf(GTK_IMAGE(old_icon), pixbuf);
+	
 	child = g_list_nth(tb->children, 5)->data;
 	old_icon = child->icon;
 	new_icon = create_pixmap(toolbar_win.toolbar, "tb_calc_send.xpm");
 	pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(new_icon));
 	gtk_image_set_from_pixbuf(GTK_IMAGE(old_icon), pixbuf);
+	
 	child = g_list_nth(tb->children, 6)->data;
 	old_icon = child->icon;
 	new_icon = create_pixmap(toolbar_win.toolbar, "tb_mkdir.xpm");
 	pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(new_icon));
 	gtk_image_set_from_pixbuf(GTK_IMAGE(old_icon), pixbuf);
+	
 	child = g_list_nth(tb->children, 7)->data;
 	old_icon = child->icon;
 	new_icon = create_pixmap(toolbar_win.toolbar, "tb_trash.xpm");
 	pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(new_icon));
 	gtk_image_set_from_pixbuf(GTK_IMAGE(old_icon), pixbuf);
+	
 	child = g_list_nth(tb->children, 8)->data;
 	old_icon = child->icon;
 	new_icon = create_pixmap(toolbar_win.toolbar, "tb_refresh.xpm");
@@ -95,19 +102,9 @@ void toolbar_refresh_buttons(void)
 		return;
 
 	/* Enable or disable some buttons */
-	gtk_widget_set_sensitive(toolbar_win.button10,
-				 (ti_calc.
-				  supported_operations() & OPS_ISREADY));
-	gtk_widget_set_sensitive(toolbar_win.button11,
-				 (ti_calc.
-				  supported_operations() & OPS_DIRLIST));
-	gtk_widget_set_sensitive(toolbar_win.button12,
-				 (ti_calc.
-				  supported_operations() &
-				  OPS_RECV_BACKUP));
-	gtk_widget_set_sensitive(toolbar_win.button13,
-				 (ti_calc.
-				  supported_operations() &
-				  OPS_SEND_BACKUP));
+	gtk_widget_set_sensitive(toolbar_win.button10, (ti_calc.supported_operations() & OPS_ISREADY));
+	gtk_widget_set_sensitive(toolbar_win.button11, (ti_calc.supported_operations() & OPS_DIRLIST));
+	gtk_widget_set_sensitive(toolbar_win.button12, (ti_calc.supported_operations() & OPS_RECV_BACKUP));
+	gtk_widget_set_sensitive(toolbar_win.button13, (ti_calc.supported_operations() & OPS_SEND_BACKUP));
 	gtk_widget_set_sensitive(toolbar_win.button14, !ti_calc.is_silent);
 }
