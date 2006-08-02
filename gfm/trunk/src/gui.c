331a76d7bc082fb7deb610542ec738dc0b26647d
Tyler Cassidy <nexon89@gmail.com>
2006-08-02 23:47:06 +0000

    0. Fixed Signal Connection! (Just missing flags) 1. Fixed GUI Quiter, was a big
    git-svn-id: http://svn.tilp.info/repos/tilp@2650

--- gfm/trunk/src/gui.c
+++ gfm/trunk/src/gui.c
@@ -3,12 +3,12 @@
   Copyright (C) 2006 Tyler Cassidy
   Copyright (C) 2006 Romain Lievin
   04/06/06 17:04 - gui.c
-  
+
   This program is free software you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation either version 2 of the License, or
   (at your option) any later version.
- 
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -16,7 +16,7 @@
 
   You should have received a copy of the GNU General Public License
   along with this program if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.         
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
 #ifdef HAVE_CONFIG_H
@@ -41,50 +41,50 @@ int launch_gfmgui(void)
     GladeXML *xml;
     GtkWidget *widget, *data;
     int pane_center, x, y;
-    
+
     // Load the GFM Dialog from gfm.glade
     xml = glade_xml_new(gfm_paths_build_glade("gfm.glade"), "gfm_dbox", NULL);
-    
+
     // Glade File Error
     if (!xml)
         g_error("Failure GFM GUI!\n", __FILE__);
-       
+
     // Connect The Symbols
     glade_xml_signal_autoconnect(xml);
-    
+
     // Retrieve the rom8x_dialog widget
-    widget = glade_xml_get_widget(xml, "gfm_dbox");   
-    
+    widget = glade_xml_get_widget(xml, "gfm_dbox");
+
     // Global Widget Access
     gfm_widget.group_tree = glade_xml_get_widget(xml, "group_tree"); // Group Tree
     gfm_widget.comp_tree = glade_xml_get_widget(xml, "folder_tree"); // Folder Tree
     gfm_widget.current_folder = glade_xml_get_widget(xml, "folder_label"); // Current Folder Label
     gfm_widget.group_files = glade_xml_get_widget(xml, "groupfile_numfiles_label"); // # files in Group
     gfm_widget.group_filesize = glade_xml_get_widget(xml, "groupfile_filesize_label"); // Size of Group
-    
+
     // Set it up so its 50/50
     data = glade_xml_get_widget(xml, "hpaned1");
     gtk_window_get_size(GTK_WINDOW(widget), &x, &y);
     pane_center = (x - 25) / 2;
     gtk_paned_set_position(GTK_PANED(data), pane_center);
-    
+
     // Setup the Group Tree
     // todo
-    
+
     // Setup the Folder Tree
     folder_tree_init(); // Init
     folder_tree_refresh(); // Refresh Folder Tree View
-    
+
     // Show the Widget
     gtk_widget_show(widget);
-    
+
     // Return
     return 0;
 }
 
 /* Callback Functions */
 /* The Destroy Dialog Event */
-GLADE_CB void on_gfm_dbox_destroy(GtkObject *object, gpointer user_data)
+GLADE_CB void gfm_main_destroy(GtkObject *object, gpointer user_data)
 {
          // Quit Main GTK Loop
          gtk_main_quit();
