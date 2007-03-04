0415ff2960e5273a733ccb3099e8d4008cb5444e
Romain Liévin <roms@lievin.net>
2007-03-04 13:42:47 +0000

    Added a small information text about ROM dumping.
    git-svn-id: http://svn.tilp.info/repos/tilp@3231

--- tilp/trunk/src/tilp_calcs.c
+++ tilp/trunk/src/tilp_calcs.c
@@ -293,6 +293,8 @@ int tilp_calc_rom_dump(void)
 	gint ret, err;
 	gchar tmp_filename[MAXCHARS];
 
+	gif->msg_box1(_("Information"), _("ROM contents is copyrighted by Texas Instruments. You are not allowed to copy and/or distribute any ROM image! For more informations, see the License Agreement provided with any FLASH OS upgrade."));
+
 	ret = gif->msg_box4(_("Warning"), _("An assembly program is about to be sent on your calculator.\nIf you have not made a backup yet, you should do one before\nproceeding with ROM dumping...\n\nFor the way of proceeding, take a look at the TiLP manual \n(especially if you have a USB cable)."));
 	if(ret != BUTTON1)
 		return -1;
