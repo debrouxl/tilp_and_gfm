0b90f52a5fb7696b671868dcf2d80960baed5c29
Romain Liévin <roms@lievin.net>
2007-03-13 13:43:11 +0000

    Added missing folder.
    git-svn-id: http://svn.tilp.info/repos/tilp@3237

---
+++ tilp/trunk/build/InnoSetup/Output/update.bat
@@ -0,0 +1,7 @@
+rm Release.txt
+rm ReadMe.txt
+rm Readme_Win32.txt
+copy C:\sources\roms\tilp2\README ReadMe.txt
+copy C:\sources\roms\tilp2\RELEASE Release.txt
+copy C:\sources\roms\tilp2\README.WIN32 Readme_Win32.txt
+cls
