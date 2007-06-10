2af4ae23e96592cbabb953949e2c37aae97e3e9b
Romain Liévin <roms@lievin.net>
2007-06-10 12:11:02 +0000

    Doesn't send idlist.8xidl pseudo-variable.
    git-svn-id: http://svn.tilp.info/repos/tilp@3401

--- tilp/trunk/src/tilp_slct.c
+++ tilp/trunk/src/tilp_slct.c
@@ -219,6 +219,9 @@ void tilp_local_contents_load(void)
 		{
 			FileEntry *fe0 = ptr->data;
 
+			if(!g_strcasecmp(tifiles_fext_get(fe0->name), "8xidl"))
+				continue;	// doesn't send this pseudo-variable
+
 			if(tifiles_file_is_single(fe0->name))
 			{
 				FileEntry *fe1 = g_memdup(ptr->data, sizeof(FileEntry));
