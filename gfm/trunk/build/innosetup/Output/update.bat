5346ab1789f46f148b21fe524f5b3416d3f4566d
Romain Liévin <roms@lievin.net>
2007-07-06 21:08:39 +0000

    Fixed paths.
    git-svn-id: http://svn.tilp.info/repos/tilp@3632

--- gfm/trunk/build/innosetup/Output/update.bat
+++ gfm/trunk/build/innosetup/Output/update.bat
@@ -1,5 +1,5 @@
 rm ReadMe.txt
 rm ChangeLog.txt
-copy C:\sources\roms\tilp2\README ReadMe.txt
-copy C:\sources\roms\tilp2\ChangeLog ChangeLog.txt
+copy C:\sources\roms\gfm\README ReadMe.txt
+copy C:\sources\roms\gfm\ChangeLog ChangeLog.txt
 cls
