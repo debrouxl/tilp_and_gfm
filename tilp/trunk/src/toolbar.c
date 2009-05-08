8288954ba79d51b2d4dcc9ba187ef48592274db9
Romain Liévin <roms@lievin.net>
2009-05-08 17:02:20 +0000

    Removed deprecated and dead code even.
    git-svn-id: http://svn.tilp.info/repos/tilp@4255

--- tilp/trunk/src/toolbar.c
+++ tilp/trunk/src/toolbar.c
@@ -19,84 +19,15 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-#undef GTK_DISABLE_DEPRECATED
 #include <gtk/gtk.h>
-#define GTK_DISABLE_DEPRECATED
 
 #include "tilp_core.h"
-
 #include "gstruct.h"
 #include "support.h"
 
 struct toolbar_window toolbar_wnd = { 0 };
 
-void toolbar_set_images(void)
-{
-	GtkToolbar *tb = GTK_TOOLBAR(toolbar_wnd.toolbar);
-	GtkToolbarChild *child;
-	GtkWidget *old_icon, *new_icon;
-	GdkPixbuf *pixbuf;
-
-	//gdk_pixbuf_new_from_xpm_data    (const char **data);
-	if (toolbar_wnd.toolbar == NULL)
-		return;
-		
-	child = g_list_nth(tb->children, 0)->data;
-	old_icon = child->icon;
-	new_icon = create_pixmap(toolbar_wnd.toolbar, "tb_ready.png");
-	pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(new_icon));
-	gtk_image_set_from_pixbuf(GTK_IMAGE(old_icon), pixbuf);
-	
-	child = g_list_nth(tb->children, 1)->data;
-	old_icon = child->icon;
-	new_icon = create_pixmap(toolbar_wnd.toolbar, "tb_calc_screen.png");
-	pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(new_icon));
-	gtk_image_set_from_pixbuf(GTK_IMAGE(old_icon), pixbuf);
-	
-	child = g_list_nth(tb->children, 2)->data;
-	old_icon = child->icon;
-	new_icon = create_pixmap(toolbar_wnd.toolbar, "tb_calc_dirlist.png");
-	pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(new_icon));
-	gtk_image_set_from_pixbuf(GTK_IMAGE(old_icon), pixbuf);
-	
-	child = g_list_nth(tb->children, 3)->data;
-	old_icon = child->icon;
-	new_icon = create_pixmap(toolbar_wnd.toolbar, "tb_calc_backup.png");
-	pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(new_icon));
-	gtk_image_set_from_pixbuf(GTK_IMAGE(old_icon), pixbuf);
-	
-	child = g_list_nth(tb->children, 4)->data;
-	old_icon = child->icon;
-	new_icon = create_pixmap(toolbar_wnd.toolbar, "tb_calc_restore.png");
-	pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(new_icon));
-	gtk_image_set_from_pixbuf(GTK_IMAGE(old_icon), pixbuf);
-	
-	child = g_list_nth(tb->children, 5)->data;
-	old_icon = child->icon;
-	new_icon = create_pixmap(toolbar_wnd.toolbar, "tb_calc_send.png");
-	pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(new_icon));
-	gtk_image_set_from_pixbuf(GTK_IMAGE(old_icon), pixbuf);
-	
-	child = g_list_nth(tb->children, 6)->data;
-	old_icon = child->icon;
-	new_icon = create_pixmap(toolbar_wnd.toolbar, "tb_mkdir.png");
-	pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(new_icon));
-	gtk_image_set_from_pixbuf(GTK_IMAGE(old_icon), pixbuf);
-	
-	child = g_list_nth(tb->children, 7)->data;
-	old_icon = child->icon;
-	new_icon = create_pixmap(toolbar_wnd.toolbar, "tb_trash.png");
-	pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(new_icon));
-	gtk_image_set_from_pixbuf(GTK_IMAGE(old_icon), pixbuf);
-	
-	child = g_list_nth(tb->children, 8)->data;
-	old_icon = child->icon;
-	new_icon = create_pixmap(toolbar_wnd.toolbar, "tb_refresh.png");
-	pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(new_icon));
-	gtk_image_set_from_pixbuf(GTK_IMAGE(old_icon), pixbuf);
-}
-
-/* Put sensitive some buttons of the toolbar according to some options */
+/* Put some buttons of the toolbar as sensitive according to some options */
 void toolbar_refresh_buttons(void)
 {
 	CalcFeatures features;
@@ -116,37 +47,4 @@ void toolbar_refresh_buttons(void)
 		gtk_widget_hide(toolbar_wnd.button15);
 	else
 		gtk_widget_show(toolbar_wnd.button15);
-
-	/*
-	if(options.calc_model == CALC_TI89T_USB || options.calc_model == CALC_TI84P_USB)
-	{
-		GtkToolbar *tb = GTK_TOOLBAR(toolbar_wnd.toolbar);
-		GtkToolItem *item = gtk_toolbar_get_nth_item(tb, 0);
-		GtkToolButton *btn = GTK_TOOL_BUTTON(item);
-		
-		gtk_tool_button_set_label       (btn, "Connect");
-	}
-	*/
-}
-
-// 0: disabled
-// 1: send
-// 2: receive
-void toolbar_set_button(int sr)
-{
-	/*
-	GtkToolbar *tb = GTK_TOOLBAR(toolbar_wnd.toolbar);
-	GtkToolItem *item = gtk_toolbar_get_nth_item(tb, 5);
-	GtkToolButton *btn = GTK_TOOL_BUTTON(item);
-	GtkWidget *icon;
-
-	icon = create_pixmap(toolbar_wnd.toolbar, "tb_refresh.png");
-	//gtk_tool_button_set_icon_widget((GtkToolButton *)item, icon);
-	//gtk_tool_button_set_label       (btn, "foobar");
-
-	if(options.full_gui)
-		gtk_widget_set_sensitive(toolbar_wnd.button14, FALSE);
-	else
-		gtk_widget_set_sensitive(toolbar_wnd.button14, sr);
-		*/
 }
