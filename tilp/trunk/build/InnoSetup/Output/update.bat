f506d0d493028c0e7caf2252a8d8e02c195e74f8
Romain Liévin <roms@lievin.net>
2009-05-29 13:51:20 +0000

    Preparing release...
    git-svn-id: http://svn.tilp.info/repos/tilp@4257

--- tilp/trunk/build/InnoSetup/Output/update.bat
+++ tilp/trunk/build/InnoSetup/Output/update.bat
@@ -1,7 +1,7 @@
 rm Release.txt
 rm ReadMe.txt
 rm Readme_Win32.txt
-copy C:\sources\roms\tilp2\README ReadMe.txt
-copy C:\sources\roms\tilp2\RELEASE Release.txt
-copy C:\sources\roms\tilp2\README.WIN32 Readme_Win32.txt
+copy C:\lpg\tilp2\README ReadMe.txt
+copy C:\lpg\tilp2\RELEASE Release.txt
+copy C:\lpg\tilp2\README.WIN32 Readme_Win32.txt
 cls
