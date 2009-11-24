af07c05a034a18f62e68ba6a4b67b6c15b999039
Lionel Debroux <lionel_debroux@yahoo.fr>
2009-11-24 20:11:16 +0000

    tilp, gfm: fix manpage cleaner not to produce junk at the end of the file, and other cleanups. Regenerate man pages. Based on a patch by Kevin Kofler, further code simplification and cleanup.
    git-svn-id: http://svn.tilp.info/repos/tilp@4284

--- gfm/trunk/man/cleaner.c
+++ gfm/trunk/man/cleaner.c
@@ -1,5 +1,7 @@
 /*  manpage_cleaner - remove the repetition of characters
- *  Copyright (C) 2000  Romain Lievin
+ *  Copyright (C) 2000 Romain Liévin
+ *  Copyright (C) 2009 Kevin Kofler
+ *  Copyright (C) 2009 Lionel Debroux
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -19,109 +21,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-
-#define MAXCHARS 256
 
 int main(int argc, char **argv)
 {
-  char filename[MAXCHARS];
-  char filename2[MAXCHARS];
-  char filename3[MAXCHARS];
+  char *filename = NULL;
+  char *filename2 = NULL;
   FILE *in;
-  FILE *tmp;
   FILE *out;
   char buffer[3];
-  
+  size_t len;
+  int retval = 0;
+
   /* Retrieve the command line argument */
   if(argc < 2)
     {
       fprintf(stderr, "You must give a filename on the command line.\n");
-      exit(1);
+      retval = 1;
+      goto end;
+    }
+
+  len = strlen(argv[1]);
+  filename = malloc(len+1);
+  filename2 = malloc(len+5);
+  if(filename == NULL || filename2 == NULL)
+    {
+      fprintf(stderr, "Cannot allocate memory.\n");
+      retval = 1;
+      goto end;
     }
   strcpy(filename, argv[1]);
   strcpy(filename2, filename);
-  strcat(filename2, ".tmp");
-  strcpy(filename3, filename);
-  strcat(filename3, ".txt");
-  
+  strcat(filename2 + len, ".txt");
+
   fprintf(stdout, "Processing file <%s>:\n", filename);
   fprintf(stdout, "Pass 1... ");
-  
-  /* Open the file for reading */
+
+  /* Open input file */
   in = fopen(filename, "rb");
   if(in == NULL)
     {
       fprintf(stderr, "Unable to open this file: <%s>\n", filename);
-      exit(1);
+      retval = 1;
+      goto end;
     }
-  
-  /* Open a temporary file fpr writing */
+
+  /* Open output file */
   out = fopen(filename2, "wb");
   if(out == NULL)
     {
+      fclose(in);
       fprintf(stderr, "Unable to open this file: <%s>\n", filename2);
-      exit(1);
+      retval = 1;
+      goto end;
     }
-  
+
   /* Process the file for removing backspace sequences */
   while(!feof(in))
     {
       buffer[0] = fgetc(in);
+      if(feof(in)) break;
+      buffer[1] = fgetc(in);
       if(feof(in))
-	{
-	  fputc(buffer[0], out);
-	  break;
-	}
-      buffer[1]= fgetc(in);
+        {
+          fputc(buffer[0], out);
+          break;
+        }
   
       if(buffer[0] == '\b')
-	{
-	  continue; // Skip the char and BS
-	}
+        {
+          continue; // Skip the char and BS
+        }
       if(buffer[1] == '\b')
-	{
-	  fputc(fgetc(in), out); // Skip the 2 previous chars
-	  continue;
-	}
+        {
+          fputc(fgetc(in), out); // Skip the 2 previous chars
+          continue;
+        }
       fputc(buffer[0], out);
       fputc(buffer[1], out);
     }
   fprintf(stdout, "Done.\n");
-  
+
   /* Close the files */
   fclose(in);
   fclose(out);
-  
-  fprintf(stdout, "Pass 2... ");
-  
-  /* Open the temporary file and another file */
-  in = fopen(filename2, "rb");
-  if(in == NULL)
-    {
-      fprintf(stderr, "Unable to open this file: <%s>\n", filename);
-      exit(1);
-    }
-  
-  out = fopen(filename3, "wb");
-  if(out == NULL)
-    {
-      fprintf(stderr, "Unable to open this file: <%s>\n", filename2);
-      exit(1);
-    }
-  
-  /* Copy the file */
-  while(!feof(in))
-    {
-      if(feof(in)) break;
-      fputc(fgetc(in), out);
-    }
-  
-  /* Close files */
-  fclose(in);
-  fclose(out);
-  unlink(filename2);
-  fprintf(stdout, "Done.\n");
-  
-  return 0;
+
+end:
+  free(filename2);
+  free(filename);
+
+  return retval;
 }
