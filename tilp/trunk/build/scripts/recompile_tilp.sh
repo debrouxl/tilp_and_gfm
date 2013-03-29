8b95501aa7efd8315ec68d588fa01fcf0cbdb725
Lionel Debroux <lionel_debroux@yahoo.fr>
2013-03-29 12:30:37 +0000

    maintainer build scripts: make declaration-after-statement a hard error.
    git-svn-id: http://svn.tilp.info/repos/tilp@4500

--- tilp/trunk/build/scripts/recompile_tilp.sh
+++ tilp/trunk/build/scripts/recompile_tilp.sh
@@ -9,7 +9,7 @@
 PREFIX="$HOME"
 
 # Common flags and definitions.
-CCFLAGS="-Os -g3 -Wall -W -Wno-unused-parameter -Wshadow -Wwrite-strings -Wredundant-decls -Wdeclaration-after-statement -fstack-protector-all -Wstack-protector --param=ssp-buffer-size=1 -Werror=implicit-function-declaration"
+CCFLAGS="-Os -g3 -Wall -W -Wno-unused-parameter -Wshadow -Wwrite-strings -Wredundant-decls -Werror=declaration-after-statement -Werror=implicit-function-declaration -fstack-protector-all -Wstack-protector --param=ssp-buffer-size=1"
 CCPPFLAGS="-D_FORTIFY_SOURCE=2"
 
 # Configure and build the given module
